c34ef4e830ff0d1ba5071740629e1e994c5af3c3
volfor <volfor@mail.ua>
2015-04-03 12:22:28 +0300

    Added buttons and animations

--- app/src/main/java/org/fairytail/guessthesong/MainActivity.java
+++ app/src/main/java/org/fairytail/guessthesong/MainActivity.java
@@ -2,6 +2,10 @@ package org.fairytail.guessthesong;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 
 public class MainActivity extends FragmentActivity {
@@ -10,5 +14,32 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        YoYo.with(Techniques.FadeIn)
+                .duration(1500)
+                .playOn(findViewById(R.id.imageView));
+
+        YoYo.with(Techniques.SlideInRight)
+                .duration(500)
+                .playOn(findViewById(R.id.singlePlayerButton));
+
+        YoYo.with(Techniques.SlideInRight)
+                .delay(100)
+                .duration(500)
+                .playOn(findViewById(R.id.multiPlayerButton));
+    }
+
+    public void onSingleButtonClick(View view) {
+
+        //Landing
+        //BounceIn
+        //SlideInRight
+
     }
 }
