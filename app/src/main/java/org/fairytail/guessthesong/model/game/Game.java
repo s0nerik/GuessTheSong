41bc42c8eadf62540e27f03720c44d39b321efd5
sonerik <thesonerik@gmail.com>
2015-04-13 00:46:05 +0300

    Game creator.

--- app/src/main/java/org/fairytail/guessthesong/model/game/Game.java
+++ app/src/main/java/org/fairytail/guessthesong/model/game/Game.java
@@ -1,13 +1,50 @@
 package org.fairytail.guessthesong.model.game;
 
+import org.fairytail.guessthesong.model.Song;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import lombok.Builder;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
-@Builder
+@RequiredArgsConstructor
 public class Game {
     private final Difficulty difficulty;
     private final List<Quiz> quizzes;
+
+    public static class Creator {
+
+        public Game create(Difficulty.Level level, List<Song> correctSongs, List<Song> allSongs) {
+            Difficulty difficulty = Difficulty.Factory.create(level);
+            List<Quiz> quizzes = createQuizzes(difficulty, correctSongs, allSongs);
+
+            return new Game(difficulty, quizzes);
+        }
+
+        private List<Quiz> createQuizzes(Difficulty difficulty, List<Song> correctSongs, List<Song> allSongs) {
+            List<Quiz> quizzes = new ArrayList<>();
+
+            for (Song s : correctSongs) {
+                quizzes.add(new Quiz(s, shuffleSongsWithThis(allSongs, s, difficulty.getVariants()), difficulty));
+            }
+
+            return quizzes;
+        }
+
+        private List<Song> shuffleSongsWithThis(List<Song> songs, Song song, int num) {
+            List<Song> shuffledSongs = new ArrayList<>(songs);
+            Collections.shuffle(shuffledSongs);
+
+            shuffledSongs = shuffledSongs.subList(0, num - 1);
+            shuffledSongs.add(song);
+            Collections.shuffle(shuffledSongs);
+
+            return shuffledSongs;
+        }
+
+    }
+
 }
--- app/src/main/java/org/fairytail/guessthesong/model/game/Quiz.java
+++ app/src/main/java/org/fairytail/guessthesong/model/game/Quiz.java
@@ -2,21 +2,24 @@ package org.fairytail.guessthesong.model.game;
 
 import org.fairytail.guessthesong.model.Song;
 
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class Quiz {
 
-    private final Song song;
+    private final Song correctSong;
+    private final List<Song> variants;
 
-    private final int duration;
+    private final Difficulty difficulty;
 
     private long startTime;
     private long endTime;
 
     public void start() {
         startTime = System.currentTimeMillis();
-        endTime = startTime + duration;
+        endTime = startTime + difficulty.getSongDuration();
     }
 
 }
