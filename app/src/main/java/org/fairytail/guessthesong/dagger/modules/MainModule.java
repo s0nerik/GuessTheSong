cc4b97de2875c9fcb833da01cae714f8a1e77434
sonerik <thesonerik@gmail.com>
2015-04-24 11:35:29 +0300

    Quiz "time is over" event.

--- app/src/main/java/org/fairytail/guessthesong/dagger/modules/MainModule.java
+++ app/src/main/java/org/fairytail/guessthesong/dagger/modules/MainModule.java
@@ -6,6 +6,7 @@ import com.squareup.otto.ThreadEnforcer;
 import org.fairytail.guessthesong.App;
 import org.fairytail.guessthesong.activities.MainActivity;
 import org.fairytail.guessthesong.bus.MainThreadBus;
+import org.fairytail.guessthesong.model.game.Quiz;
 import org.fairytail.guessthesong.player.MusicPlayer;
 import org.fairytail.guessthesong.player.Player;
 import org.fairytail.guessthesong.prefs.PrefManager;
@@ -17,7 +18,8 @@ import dagger.Provides;
 
 @Module(
         injects = {
-                MainActivity.class
+                MainActivity.class,
+                Quiz.class
         },
         library = true
 )
--- app/src/main/java/org/fairytail/guessthesong/model/game/Quiz.java
+++ app/src/main/java/org/fairytail/guessthesong/model/game/Quiz.java
@@ -1,13 +1,24 @@
 package org.fairytail.guessthesong.model.game;
 
+import android.os.Handler;
+
+import com.squareup.otto.Bus;
+
+import org.fairytail.guessthesong.dagger.Daggered;
+import org.fairytail.guessthesong.events.QuizTimeOverEvent;
 import org.fairytail.guessthesong.model.Song;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class Quiz {
+public class Quiz extends Daggered {
+
+    @Inject
+    Bus bus;
 
     private final Song correctSong;
     private final List<Song> variants;
@@ -20,6 +31,9 @@ public class Quiz {
     public void start() {
         startTime = System.currentTimeMillis();
         endTime = startTime + difficulty.getSongDuration();
+
+        new Handler().postDelayed(() ->
+                bus.post(new QuizTimeOverEvent(this)), difficulty.getSongDuration());
     }
 
 }
