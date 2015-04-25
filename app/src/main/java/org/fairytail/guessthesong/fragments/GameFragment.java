170c3f84c7ecc3c6c3775f2618c1386e2991c624
volfor <volfor@mail.ua>
2015-04-25 18:52:32 +0300

    Added buttons to select the answer.

--- app/src/main/java/org/fairytail/guessthesong/fragments/GameFragment.java
+++ app/src/main/java/org/fairytail/guessthesong/fragments/GameFragment.java
@@ -3,16 +3,25 @@ package org.fairytail.guessthesong.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import org.fairytail.guessthesong.R;
 import org.fairytail.guessthesong.dagger.Injector;
+import org.fairytail.guessthesong.model.game.Quiz;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class GameFragment extends Fragment{
+
+    @InjectView(R.id.flMain)
+    FrameLayout flMain;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,11 +39,32 @@ public class GameFragment extends Fragment{
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        //TODO:addVariants(view, quiz);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         ButterKnife.reset(this);
+    }
+
+    public void addVariants(View view, Quiz quiz) {
+        int position = view.getHeight() / 2;
+
+        FrameLayout.LayoutParams lParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT, Gravity.CENTER);
+
+        for (int i = 0; i < quiz.getVariants().size(); i++) {
+            Button btnVariant = new Button(this.getActivity());
+            btnVariant.setText(quiz.getVariants().get(i).getArtist() + " - " + quiz.getVariants().get(i).getTitle());
+            btnVariant.setY(position);
+
+            final int finalI = i;
+            btnVariant.setOnClickListener(v -> quiz.check(quiz.getVariants().get(finalI)));
+
+            flMain.addView(btnVariant, lParams);
+            position += 50;
+        }
     }
 }
